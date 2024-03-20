4d7f9e2858e6c34bc988fd5fc0eecc65ee8acae1
AMIR_CODER <amircoder86@gmail.com>
2024-03-19 19:39:10 -0700

    start

---
+++ src/main/java/org/example/javafx/HelloController.java
@@ -0,0 +1,21 @@
+package org.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+
+public class HelloController {
+
+    @FXML
+    private Label lb_rahnama;
+
+    @FXML
+    private Tab tab_class;
+
+    @FXML
+    private Tab tab_teachers;
+
+    @FXML
+    private Label welcomeText;
+
+}
